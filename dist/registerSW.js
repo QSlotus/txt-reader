eb0f808d221612adf4ecf4e4518938771e189a5e
nalomu <2556468180@qq.com>
2023-03-03 17:27:12 +0800

    build

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/txt-reader/sw.js', { scope: '/txt-reader/' })})}
