961c505c03812f300107a2aa4345ae23fb741150
nalomu <2556468180@qq.com>
2023-03-03 17:11:54 +0800

    test pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,35 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // console.log(process.env)
 console.log(process.env.BUILD_PLATFORM)
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BUILD_PLATFORM === 'GH' ? '/txt-reader/' : '/',
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'My Awesome App',
+      short_name: 'MyApp',
+      description: 'My Awesome App description',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
